61c137022a6cd463d90e22337630e95e8c372cff
victoriaviv <dufatanyevictoria2@gmail.com>
2025-03-20 13:28:32 +0200

    changes

--- routes/indexRouting.js
+++ routes/indexRouting.js
@@ -3,6 +3,7 @@ import express from "express"
 import userRouter from "./userPath.js";
 import blogRouter from "./blogPath.js";
 import commentRouter from "./commentPath.js";
+import ContactRouter from "./contactPath.js";
 
 
 const mainRouter=express.Router();
@@ -10,5 +11,6 @@ const mainRouter=express.Router();
 mainRouter.use("/user",userRouter);
 mainRouter.use("/blog",blogRouter);
 mainRouter.use("/comment",commentRouter);
+mainRouter.use("/contact",ContactRouter);
 
 export default mainRouter;
